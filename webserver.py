22dbca95aa3aa388a75d5a6f80ed8e73769c4a3c
Lee Garam <agerio100@naver.com>
2024-03-10 00:01:45 +0900

    Basic frontend
    Just for being pretty

--- webserver.py
+++ webserver.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# Mount the static directory to serve CSS file
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return FileResponse("static/index.html", media_type="text/html")
